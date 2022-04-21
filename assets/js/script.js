152025a924d6bba546453480403c951bdc85ff64
Azalee1050 <jordan2officiel@gmail.com>
2022-04-22 00:28:13 +0100

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
+const humbger = document.querySelector(".humbger");
+const humbgermenu = document.querySelector(".humbgermenu");
 
+humbger.onclick = function() {
+    humbger.classList.toggle("active");
+    humbgermenu.classList.toggle("active");
+}
 window.addEventListener("load", ShowAff)
 
 var loading;
 
 function ShowAff () {
-    loading = setTimeout(ShowPages, 10000);
+    loading = setTimeout(ShowPages, 1);
 }
 
 function ShowPages() {
