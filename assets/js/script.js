2c82ce26b00e8e61f4f01122f6e53fc70fa97285
Azalee1050 <jordan2officiel@gmail.com>
2022-04-24 21:37:10 +0100

    master

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 const humbger = document.querySelector(".humbger");
 const humbgermenu = document.querySelector(".humbgermenu");
+const cursor = document.querySelector(".cursor");
 
+
+
+document.addEventListener("mousemove", function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+})
 humbger.onclick = function() {
     humbger.classList.toggle("active");
     humbgermenu.classList.toggle("active");
 }
 window.addEventListener("load", ShowAff)
 
+
 var loading;
 
 function ShowAff () {
-    loading = setTimeout(ShowPages, 10000);
+    loading = setTimeout(ShowPages, 10);
 }
 
 function ShowPages() {
